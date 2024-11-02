359e22d5aa9ef7e10f1082b0bea5c70da0db68c8
RIMZA ASAD <rimzaasad28@gmail.com>
2024-11-02 11:16:35 +0500

    Normalize line endings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
+
 
 
 
